ab2b70f8ddcea6a0d814bf3ff0bfe6fc09d8cb74
Maxgg21 <lionel.gegena@gmail.com>
2023-06-20 23:38:39 -0300

    Subiendo aplicacion con React

---
+++ src/componentes/TirasDePeliculas.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Pelicula from './Pelicula'
+
+function TirasDePeliculas() {
+    return (
+        <div>
+            <Pelicula />
+            <Pelicula />
+            <Pelicula />
+            <Pelicula />
+        </div>
+    );
+  }
+export default TirasDePeliculas;
